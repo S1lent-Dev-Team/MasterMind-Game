a4810a7b3d760243f4ca63268868310eae339b2e
Jonas <eisele-jonas@web.de>
2024-04-15 15:00:35 +0200

    Push

--- org/tgi11/mastermind/Steuerung.java
+++ org/tgi11/mastermind/Steuerung.java
@@ -1,4 +1,21 @@
 package org.tgi11.mastermind;
 
+import java.awt.*;
+
 public class Steuerung {
+
+    public final Color[] answer;
+
+    public Steuerung(Color[] answer) {
+        this.answer = answer;
+    }
+    public Color[][] getHistory(){
+        return null;
+    }
+    public Color[][] getReturnHistory(){
+        return null;
+    }
+    public void guess(Color[] guess){
+
+    }
 }
