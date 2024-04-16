b4b25d55c3ff8ebc6fa231b6ffa505532355a903
BuildTools <unconfigured@null.spigotmc.org>
2024-04-16 20:14:00 +0200

    Scanner importiert

--- org/tgi11/mastermind/CLI.java
+++ org/tgi11/mastermind/CLI.java
@@ -1,5 +1,14 @@
 package org.tgi11.mastermind;
+import java.util.Scanner;
 
+//Felix
 public class CLI {
 
+    public CLI(){
+        Scanner ans = new Scanner(System.in);
+    }
+
+    public void abfrage(){
+
+    }
 }
