aba026acfad91f9fa39023b2ea1e42a988fd30aa
CollinGHG <collinghg@hyperiummc.de>
2024-04-16 20:55:26 +0200

    Update

--- org/tgi11/mastermind/Solver.java
+++ org/tgi11/mastermind/Solver.java
@@ -1,4 +1,6 @@
 package org.tgi11.mastermind;
 
+// Collin
 public class Solver {
+    
 }
--- org/tgi11/mastermind/Steuerung.java
+++ org/tgi11/mastermind/Steuerung.java
@@ -1,10 +1,14 @@
 package org.tgi11.mastermind;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Steuerung {
 
     public final Color[] answer;
+
+    private static Color[] secretCode = new Color[8];
 
     public Steuerung(Color[] answer) {
         this.answer = answer;
@@ -17,5 +21,26 @@ public class Steuerung {
     }
     public void guess(Color[] guess){
 
+    }
+    private static int countCorrectColors(Color[] secretCode, Color[] guess) {
+        int count = 0;
+        List secretList = new ArrayList<>(Arrays.asList(secretCode));
+        for (Color color : guess) {
+            if (secretList.contains(color)) {
+                count++;
+                secretList.remove(String.valueOf(color));
+            }
+        }
+        return count;
+    }
+
+    private static int countCorrectPositions(Color[] guess) {
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            if (secretCode[i] == guess[i]) {
+                count++;
+            }
+        }
+        return count;
     }
 }
