bb18a95bb783de61f4d914a2ec5363c830817c25
verz0ckt <146005916+verz0ckt@users.noreply.github.com>
2024-04-17 08:47:23 +0200

    Update Main.java

--- org/tgi11/mastermind/Main.java
+++ org/tgi11/mastermind/Main.java
@@ -1,8 +1,21 @@
 package org.tgi11.mastermind;
 
 public class Main {
-
+private Display d;
     public static void main(String[] args){
-
-    }
+		Steuerung steuerung = new Steuerung();
+		System.out.println("With GUI?");
+		System.out.println("Yes(1) No(0)");
+		Scanner s = new Scanner(System.in);
+		boolean shouldgui = false;
+		if(s.hasNextInt()){
+			if(s.getNextInt()== "1"){
+				shouldgui = true;
+			}
+		}
+		if(shouldgui){
+			d = new GUI(steuerung);
+    	}else{
+			d = new CLI(steuerung);
+		}
 }
