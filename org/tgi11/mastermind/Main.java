0f767ffbf06e9ca25c19cef2a4b9f94a57f123a8
verz0ckt <146005916+verz0ckt@users.noreply.github.com>
2024-04-17 09:17:54 +0200

    Update Main.java

--- org/tgi11/mastermind/Main.java
+++ org/tgi11/mastermind/Main.java
@@ -3,7 +3,7 @@ package org.tgi11.mastermind;
 public class Main {
 private Display d;
     public static void main(String[] args){
-		Steuerung strg = new Steuerung();
+		Steuerung strg = new Steuerung(d);
 		System.out.println("With GUI?");
 		System.out.println("Yes(1) No(0)");
 		Scanner s = new Scanner(System.in);
