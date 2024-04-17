dbd13f640d9138edb55a71c0230cb4b332a24bcc
verz0ckt <146005916+verz0ckt@users.noreply.github.com>
2024-04-17 09:10:51 +0200

    Update Steuerung.java

--- org/tgi11/mastermind/Steuerung.java
+++ org/tgi11/mastermind/Steuerung.java
@@ -5,37 +5,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Steuerung {
-	private Color[] answer;
+				private Color[] answer;
     private Color[][] history = new Color[8][4];
-	private Solver solv;
-	private int guessesmade = 0;
+				private Solver solv;
+				private int guessesmade = 0;
 
     public Steuerung() {
-		solv = new Solver();
+					solv = new Solver();
     }
+				public getGuesscount(){
+						return guessesmade;
+				}
     public Color[][] getHistory(){
         return null;
     }
-	public void setAnswer(Color[] answer){
-		if(this.answer == null){
-		this.answer = answer
-		}
-	}
+				public void setAnswer(Color[] answer){
+						if(this.answer == null){
+						this.answer = answer
+						}
+				}
 
     public void start(boolean playerguessing){
-		solv.start(!playerguessing);
+						solv.start(!playerguessing);
     }
-	public void newGame(){
-	answer = null;
-	history = new Color[8][4];
-	}
+				public void newGame(){
+						answer = null;
+						history = new Color[8][4];
+				}
     public boolean guess(Color[] guess){
-		history[guessesmade] = guess;
-		guessesmade++;
-		if(guess == answer){
-			return true;
-		}
-		return false;
+						history[guessesmade] = guess;
+						guessesmade++;
+						if(guess == answer){
+								return true;
+						}
+						return false;
     }
 
 
