bf13122b07d8b82447bf33dfb73d81b0d44b6eab
Moonfxllxn <suetterlin.felix@web.de>
2024-04-17 15:20:30 +0200

    CLI Abfrage geaddet (Nicht ganz fertig), Scanner in main importiert

--- org/tgi11/mastermind/CLI.java
+++ org/tgi11/mastermind/CLI.java
@@ -2,14 +2,32 @@ package org.tgi11.mastermind;
 import java.util.Scanner;
 
 //Felix
-public class CLI extends Display{
-    Scanner s;
-    public CLI(Steuerung strg){
-		super(strg);
-        s = new Scanner(System.in);
+public class CLI extends Display {
+
+
+    public CLI(Steuerung strg) {
+        super(strg);
     }
 
-    public void abfrage(){
 
+    public void abfrage() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("Wie wollen sie spielen? (PC = 1, Sie = 2"); //Entscheidung zwischen Solver und selber raten
+        if (s.hasNextInt()) {
+            int game = s.nextInt();
+            switch (game) {
+                case 1:
+                    System.out.println("Der Computer rät");
+                    break;
+                case 2:
+                    System.out.println("Sie raten");
+                    break;
+
+                default:
+                    System.out.println("Ungültige Eingabe");
+            }
+            s.close();
+        }
     }
 }
--- org/tgi11/mastermind/Main.java
+++ org/tgi11/mastermind/Main.java
@@ -1,21 +1,23 @@
 package org.tgi11.mastermind;
+import java.util.Scanner;
 
 public class Main {
 private Display d;
-    public static void main(String[] args){
-		Steuerung strg = new Steuerung(d);
-		System.out.println("With GUI?");
-		System.out.println("Yes(1) No(0)");
-		Scanner s = new Scanner(System.in);
-		boolean shouldgui = false;
-		if(s.hasNextInt()){
-			if(s.getNextInt()== "1"){
-				shouldgui = true;
-			}
-		}
-		if(shouldgui){
-			d = new GUI(strg);
-    	}else{
-			d = new CLI(strg);
-		}
+    public static void main(String[] args) {
+        Steuerung strg = new Steuerung(d);
+        System.out.println("With GUI?");
+        System.out.println("Yes(1) No(0)");
+        Scanner s = new Scanner(System.in);
+        boolean shouldgui = false;
+        if (s.hasNextInt()) {
+            if (s.getNextInt() == "1") {
+                shouldgui = true;
+            }
+        }
+        if (shouldgui) {
+            d = new GUI(strg);
+        } else {
+            d = new CLI(strg);
+        }
+    }
 }
