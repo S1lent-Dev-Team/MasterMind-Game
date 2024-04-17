796774e2f7477ec9cbd6a660329e499518286871
verz0ckt <146005916+verz0ckt@users.noreply.github.com>
2024-04-17 12:18:34 +0200

    Update Solver.java

--- org/tgi11/mastermind/Solver.java
+++ org/tgi11/mastermind/Solver.java
@@ -2,8 +2,9 @@ package org.tgi11.mastermind;
 
 // Collin
 public class Solver {
-	
-	public Solver(){
+	private Steuerung strg;
+	public Solver(Steuerung strg){
+		this.strg = strg;
 	}
 	public void start(boolean comguessing){
 	}
