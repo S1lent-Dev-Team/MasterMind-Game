86b40cffb03ce9f7b5fa3e7ffc461ec839a7605d
verz0ckt <146005916+verz0ckt@users.noreply.github.com>
2024-04-17 08:05:06 +0200

    Update Solver.java

--- org/tgi11/mastermind/Solver.java
+++ org/tgi11/mastermind/Solver.java
@@ -2,5 +2,9 @@ package org.tgi11.mastermind;
 
 // Collin
 public class Solver {
-    
+    public final boolean isguessing;
+	
+	public Solver(boolean isguessing){
+		this.isguessing = isguessing;
+	}
 }
