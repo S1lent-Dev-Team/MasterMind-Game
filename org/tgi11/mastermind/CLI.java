8f2824da534ba9a005b58aa175b9cbb69bbc24d0
Moonfxllxn <unconfigured@null.spigotmc.org>
2024-05-02 20:36:08 +0200

    Minor mistake fix

--- org/tgi11/mastermind/CLI.java
+++ org/tgi11/mastermind/CLI.java
@@ -38,9 +38,10 @@ public class CLI extends Display {
 
         }
 
-        System.out.println(" ");
-        System.out.println("Bitte waehlen sie Ihren Code: (ABCD (Eine Zahl zwischen 1 und 8))");
+
         if(!playerguessing) {
+            System.out.println(" ");
+            System.out.println("Bitte waehlen sie Ihren Code: (ABCD (Eine Zahl zwischen 1 und 8))");
             for (int i = 0; i < temp.length; i++) {
                 while (!playerguessing) {
                     if (s.hasNextInt()) {
