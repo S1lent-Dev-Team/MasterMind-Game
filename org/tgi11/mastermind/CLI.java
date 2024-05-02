0539b83c2e7131638fb7e058a02ef394596b837b
BuildTools <unconfigured@null.spigotmc.org>
2024-05-02 20:18:15 +0200

    CLI fertig

--- org/tgi11/mastermind/CLI.java
+++ org/tgi11/mastermind/CLI.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 
 //Felix
 public class CLI extends Display {
-Scanner s;
+    Scanner s;
     private int[] temp = new int[4];
+    private int[] guess = new int[4];
 
     public CLI(Steuerung strg) {
         super(strg);
@@ -22,66 +23,73 @@ Scanner s;
             }else{
                 s.next();
             }
-                if(game == 1) {
-                    System.out.println("Der Computer raet");
-                    playerguessing = false;
-                    break;
-                } else if (game == 2) {
-                    System.out.println("Sie raten");
-                    playerguessing = true;
-                    break;
+            if(game == 1) {
+                System.out.println("Der Computer raet");
+                playerguessing = false;
+                break;
+            } else if (game == 2) {
+                System.out.println("Sie raten");
+                playerguessing = true;
+                break;
 
-                }else{
-                        System.out.println("Illegal Arguments");
-                }
+            }else{
+                System.out.println("Illegal Arguments");
+            }
 
         }
 
         System.out.println(" ");
         System.out.println("Bitte waehlen sie Ihren Code: (ABCD (Eine Zahl zwischen 1 und 8))");
-    if(!playerguessing) {
-        for (int i = 0; i < temp.length; i++) {
-            while (!playerguessing) {
-                if (s.hasNextInt()) {
-                    int t;
-                    t = s.nextInt();
-                    if (t >= 1 && t <= 8) {
-                        temp[i] = t;
+        if(!playerguessing) {
+            for (int i = 0; i < temp.length; i++) {
+                while (!playerguessing) {
+                    if (s.hasNextInt()) {
+                        int t;
+                        t = s.nextInt();
+                        if (t >= 1 && t <= 8) {
+                            temp[i] = t;
+                        }
+                    } else {
+                        System.out.println("Illegal Arguments");
+                        s.next();
                     }
-                } else {
-                    System.out.println("Illegal Arguments");
-                    s.next();
                 }
-
-                //TODO:ask for the code with numbers 1-8
-                //TODO:send code to strg with strg.setAnswer(); and then break;
             }
+            strg.setAnswer(temp);
         }
-        strg.setAnswer(temp);
-    }
 
 
 
 
-    strg.start(playerguessing);
-    while(strg.isRunning()){
-		//gameloop
-        if(playerguessing){
-
-
-            //TODO:player guessing process. maybe change if to while if needed;
-            //TODO:use strg.guess(); for a guess;
+        strg.start(playerguessing);
+        while(strg.isRunning()){
+            //game loop
+            System.out.println(" ");
+            System.out.println("Nennen Sie einen code zum Raten (Zahlen zwischen 1 und 8): ");
+            if(playerguessing){
+                for(int i = 0; i < guess.length;i++){
+                    if(s.hasNextInt()) {
+                        int g = s.nextInt();
+                        if(g >= 1 && g <= 8) {
+                            guess[i] = g;
+                        }
+                    } else {
+                        System.out.println("Illegal Arguments");
+                        s.next();
+                    }
+                }
+            }
+            strg.guess(guess);
         }
-	}
-
     }
 
     @Override
     public void draw() {
         strg.getLatestGuess();
     }
-    public void drawArray(){
+
+    public void drawArray() {
 
     }
-	
+
 }
