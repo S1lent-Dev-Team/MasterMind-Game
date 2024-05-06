cc0d1b74ae3d5fd0f329293b9fa336f0077faa52
Moonfxllxn <unconfigured@null.spigotmc.org>
2024-05-06 15:53:59 +0200

    Kommentare
    (America ya :D)

--- org/tgi11/mastermind/Solver.java
+++ org/tgi11/mastermind/Solver.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 public class Solver {
 	private static Steuerung strg;
 	private static Random rand = new Random();
+	private boolean intelGuess = true;
 
 	public Solver(Steuerung strg) {
 		this.strg = strg;
@@ -16,11 +17,16 @@ public class Solver {
 
 	public void start(boolean comguessing) {
 		if (comguessing) {
-			randomGuess();
-			while (strg.isRunning()){
+			if(intelGuess){
+			strg.guess(new int[]{1,1,2,2});
+			}
+			while (strg.isRunning()) {
 				systemWait();
-				//intelligentGuess();
-				randomGuess();
+				if (intelGuess) {
+					intelligentGuess();
+				} else{
+					randomGuess();
+				}
 			}
 		} else {
 			int[] code = generateRandomCode();
@@ -55,11 +61,24 @@ public class Solver {
 		int[] guess = new int[4];
 		int[][] history = strg.getHistory();
 		int[] latestGuess = strg.getLatestGuess();
-		int correctPositionAndColor = latestGuess[4];
-		int correctColor = latestGuess[5];
+		int correctPositionAndColor = latestGuess[4]; //x
+		int correctColor = latestGuess[5]; //y
+		//p = 4
 		// Heuristische Methode zur Generierung des nächsten Zugs
 		// Beispiel: Wähle die häufigsten Farben aus, die nicht bereits in der letzten Vermutung enthalten waren
 
+
+		/*
+		s = 4096
+		1er guess = 1122
+
+		Falsche Farben -> Nichtmehr benutzen
+		Wenn nicht position: S - guess code z.B. rot,gelb,grün,blau -> Stimmt nur Farbe: Dump codes mit R,G,GR,B an falscher Stelle -> Speicher Farbe dann dump
+		Wenn x >= 1 -> dump nicht
+		Wenn y = 0 ->dump
+
+		=> Mach guess basierend auf geg. Pos + Farben
+		*/
 		strg.guess(guess);
 	}
 
