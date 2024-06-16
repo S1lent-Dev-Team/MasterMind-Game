503eb4032bfc1bc2704dbeff54aa2d0bbc2e9341
Cellum <78751152+c377um@users.noreply.github.com>
2024-06-16 16:42:16 +0200

    Add files via upload

--- org/tgi11/mastermind/GUI.java
+++ org/tgi11/mastermind/GUI.java
@@ -1,21 +1,24 @@
 package org.tgi11.mastermind;
 
-
 import org.tgi11.mastermind.gui.Frame;
 
+import java.util.Scanner;
+
 public class GUI extends Display{
-	public GUI(Steuerung strg){
-		super(strg);
+    private Frame frame;
+    public GUI(Steuerung strg){
+        super(strg);
 }
 
-	@Override
-	public void start() {
-			Frame f = new Frame(this,strg);
-	}
+    @Override
+    public void start() {
+            Frame f = new Frame(this, strg);
+    }
 
-	@Override
-	public void draw() {
-
-	}
+    @Override
+    public void draw() {
+        frame.updateHistory();
+        frame.updateStatusLabel();
+    }
 
 }
